a52fcd45d71176dac0332714900836bd777c4eeb
Jedrzej <jedrzej.kus@icloud.com>
2021-03-07 21:37:53 +0100

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Jędrzej Kuś on 07/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
